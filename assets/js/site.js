1c6b02f2be23aa518bda6787fa3b9ab2a882a350
DenGoatetGamer <MingusMattino@gmail.com>
2026-09-14 10:34:06 +0200

    oinske main

---
+++ assets/js/site.js
@@ -0,0 +1,39 @@
+//#region Model
+function lavønske(){
+
+}
+//#endregion
+
+//#region View
+function main(){
+
+    // laver alle Elementerne
+    const overskrift=document.createElement("h1")
+    const inspKnap=document.createElement("button")
+    const søgfelt=document.createElement("input")
+    const søgknap=document.createElement("button")
+    const ønsketext=document.createElement("p")
+    const ønsker=document.createElement("section")
+    }
+
+function card(){
+
+    const wishsection=document.getElementById("ønsker")
+    const søgfelt=document.getElementById("søgfelt").value
+
+    const ønsketext=document.createElement("p")
+    const sletknap=document.createElement("button")
+
+    
+    
+    ønsketext.innerText = søgfelt
+
+    wishsection.appendChild()
+
+    
+
+}
+//#endregion 
+
+//#region Control
+//#endregion
